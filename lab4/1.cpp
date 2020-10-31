e89d9c01cc34866ffd547a0e7ac3e1d426a294c5
Katarzyna <kataszl768@student.polsl.pl>
2020-10-31 09:36:21 +0100

    pointers1

--- lab4/1.cpp
+++ lab4/1.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 
-using namespace str;
+using namespace std;
 
 
 int main()
 {
+    int variabble=8;
+
+    int* ptr; //empty ---> null pionter
+
+    cout<<ptr<<endl;
+    cout<<*ptr<<endl;
 
 
     return 0;
