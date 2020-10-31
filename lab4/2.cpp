d26fd099ff4565850fe2e8d779231c4ab00d558b
Katarzyna <kataszl768@student.polsl.pl>
2020-10-31 10:21:41 +0100

    ptr2

--- lab4/2.cpp
+++ lab4/2.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <array>
 using namespace std;
 
 int main()
 {
+    array<int,4> numbers;
 
+    for (int i=0;i<4;i++){
+        numbers[i]=i;
+    }
+
+    //cout<<&numbers[0]<<endl;
+    //cout<<&numbers[1]<<endl;
+    //cout<<&numbers[2]<<endl;
+    //cout<<&numbers[3]<<endl;
+    //cout<<&numbers[4]<<endl;
+
+    int *ptr_numbers=&numbers[0];
+    ptr_numbers+=3;
+    cout<<*ptr_numbers<<endl;
 
     return 0;
 }
