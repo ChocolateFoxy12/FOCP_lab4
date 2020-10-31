163bfa259a614fe6f907be8c46adb3a6cd657c10
Katarzyna <kataszl768@student.polsl.pl>
2020-10-31 11:42:28 +0100

    map2

--- lab4/4.cpp
+++ lab4/4.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
 #include <vector>
+#include <map>
+#include <fstream>
+#include <string>
 using namespace std;
+
 
 int main()
 {
+    map<string, int> dictionary;
 
+    ifstream input("lotem.txt");
 
-    return 0;
+    if (input.is_open()){
+        string word;
+
+        while (getline(input, word)){
+           if (dictionary.find(word)==dictionary.end()){
+            dictionary.insert({word, 1});
+           }
+           else{
+            dictionary[word]++;
+           }
+        }
+    }
+
+ string tmp;
+
+ cout<<"choose a word: "<<endl;
+ cin>>tmp;
+
+ cout<<"the word "<<tmp<<" appears "<<dictionary[tmp]<<" times."<<endl;
+
+ return 0;
 }
